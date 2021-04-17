267d4e9f4f710ccae30b8f47e5b016b2f74e4ee2
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-17 23:06:30 +0530

    added boiler plate/starter code for other components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/show" component={StreamShow} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
