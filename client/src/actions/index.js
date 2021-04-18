5d127a92d63887b85aefcc525fe224e27fc76cf1
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-19 02:43:12 +0530

    codes for dispatcher/reducers/actions are added. closes #12

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,16 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    EDIT_STREAM,
+    DELETE_STREAM 
+ } from './types';
+
 import { formValues } from 'redux-form';
+import StreamCreate from '../components/streams/StreamCreate';
 
 export const signIn = userId => {
     return {
@@ -16,8 +26,42 @@ export const signOut = () => {
 };
 
 // action creator for create stream, uses redux thunk
-export const createStream = (formValues) => {
+export const createStream = formValues => {
     return async (dispatch) => {
-        streams.post('/streams', formValues);
+        const response = await streams.post('/streams', formValues);
+
+        dispatch({type: CREATE_STREAM, payLoad: response.data})
     };
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({
+        type: FETCH_STREAMS,
+        payLoad: response.data
+    });
+};
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({
+        type: FETCH_STREAM,
+        payLoad: response.data
+    });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({
+        type: EDIT_STREAM,
+        payLoad: response.data
+    });
+};
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({
+        type: DELETE_STREAM,
+        payLoad: id
+    });
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamsReducer from './streamsReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: reducer
-});
+    form: reducer,
+    streams: streamsReducer
+}); 
