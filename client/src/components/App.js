ff33cc80b6add62e3365a7e5ba7e1c60365fc040
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-20 01:22:55 +0530

    code for wildcard navigation added. closes #18

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,10 +16,10 @@ const App = () => {
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/show" component={StreamShow} />
-                    <Route path="/streams/edit" component={StreamEdit} />
                     <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/edit/:id" component={StreamEdit} />
+                    <Route path="/streams/show/:id" component={StreamShow} />
+                    <Route path="/streams/delete/:id" component={StreamDelete} />
                 </div>
             </Router>
         </div>
