d3e5242cbeccbda73df33ffdfe5851df0bf1b70c
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-21 03:57:17 +0530

    code added for ShowStream component. ref #23

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
@@ -15,11 +15,13 @@ const App = () => {
             <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/edit/:id" component={StreamEdit} />
-                    <Route path="/streams/show/:id" component={StreamShow} />
-                    <Route path="/streams/delete/:id" component={StreamDelete} />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" component={StreamCreate} />
+                        <Route path="/streams/edit/:id" component={StreamEdit} />
+                        <Route path="/streams/show/:id" component={StreamShow} />
+                        <Route path="/streams/delete/:id" component={StreamDelete} />
+                    </Switch>
                 </div>
             </Router>
         </div>
