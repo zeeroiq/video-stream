1b13411392a03448561f3d9544907d6f895b6e9e
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-22 03:58:13 +0530

    code for rendering video stream  component added, closes #25

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js'
 
 class StreamShow extends React.Component{
     
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params.id;
+        this.props.fetchStream(id);
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
@@ -16,6 +28,7 @@ class StreamShow extends React.Component{
         }
         return (
             <div>
+                <video ref={ this.videoRef } style={{width: '100%'}} controls />
                 <h1>{this.props.stream.title}</h1>
                 <h5>{this.props.stream.description}</h5>
             </div>
